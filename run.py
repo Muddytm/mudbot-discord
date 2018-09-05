d788805637a6c574c1d9554422dcb9a2685abf9b
Caleb Hawkins <hawkins.caleb93@gmail.com>
2018-09-05 13:13:41 -0700

    Added on_message to add users to userdata and steal their identities while they sleep

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import asyncio
+from discord.ext import commands
 import config
 import discord
+import json
 import mudules
-from discord.ext import commands
+import os
 
 BOT_PREFIX = "!"
 TOKEN = config.app_token
@@ -16,6 +18,24 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+
+
+@client.event
+async def on_message(message):
+    """Add userdata json file and run certain functionality when a user types
+    any message, anywhere.
+    """
+    #if message.content.startswith("?"):
+    #    await client.send_message(message.channel, "Name: {}, ID: {}".format(message.author.name, message.author.id))
+
+    # Let's create a JSON file for this user.
+    if not os.path.isfile("userdata/{}_{}.json".format(message.author.name, message.author.id)):
+        with open("userdata/{}_{}.json".format(message.author.name, message.author.id), "w") as outfile:
+            user_json = {"name": message.author.name, "id": message.author.id}
+            json.dump(user_json, outfile)
+
+    # This allows us to use other commands as well.
+    await client.process_commands(message)
 
 
 @client.command(pass_context=True)
